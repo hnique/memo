dfd2e54ddf7f5b46de47ddd36ce3d43c58ec593b
test <test@test.com>
2023-07-25 18:38:50 +0900

    0725 수업

--- src/main/java/com/memo/common/FileManagerService.java
+++ src/main/java/com/memo/common/FileManagerService.java
@@ -13,7 +13,8 @@ import org.springframework.web.multipart.MultipartFile;
 public class FileManagerService {
 
 	// 실제 업로드가 된 이미지가 저장될 경로(서버)
-	public static final String FILE_UPLOAD_PATH = "C:\\Users\\PC\\Desktop\\hye\\code\\Spring_project\\memo\\workspace\\images/";
+//	public static final String FILE_UPLOAD_PATH = "C:\\Users\\PC\\Desktop\\hye\\code\\Spring_project\\memo\\workspace\\images/";
+	public static final String FILE_UPLOAD_PATH = "D:\\hyesun\\6_springproject\\memo\\workspace\\images/";
 
 	// input: MultipartFile(이미지 파일), loginId
 	// output: web image path(String)
